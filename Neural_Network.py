d36d13bf7c386651f40f44344c2862a7e5e2b603
krakencode <daltonsumrall@gmail.com>
2018-04-26 19:43:50 -0500

    The Neural network resets its weights on a call to fit()

--- Neural_Network.py
+++ Neural_Network.py
@@ -8,24 +8,34 @@ def sigmoid(x, derivative=False):
         return x * (1.0 - x)
     return 1.0 / (1 + np.exp(-x))
 
+
 def relu(x, derivative=False):
     if derivative:
         return 1
     return max(0, x)
 
+
 class Network:
-    def __init__(self, layer_structures, learning_rate=.001, activation_function=sigmoid):
 
-        inputCount = layer_structures[0]
-
-        self.inputCount = inputCount
-        layerCount = len(layer_structures)
-        self.layers = [None] * layerCount
+    def __init__(self, layer_structures, iterations=100, activation_function='sigmoid', learning_rate=.001):
+        self.layer_structures = layer_structures
         self.learning_rate = learning_rate
+        self.iterations = iterations
 
+        if activation_function.lower() == 'relu':
+            self.activation_function = relu
+        else:
+            self.activation_function = sigmoid
+
+
+    def init_network_layers(self):
+        inputCount = self.layer_structures[0]
+        layerCount = len(self.layer_structures)
+        self.layers = [None] * layerCount
         lastOutput = inputCount
+
         for i in range(layerCount):
-            outputs = layer_structures[i]
+            outputs = self.layer_structures[i]
 
             if i == 0:
                 outputs = inputCount
@@ -34,13 +44,15 @@ class Network:
             self.layers[i] = layer
             lastOutput = outputs
 
-    def predict(self, in_set, all_layers=False, classification=False):
 
+    # TODO: classification flag isn't used??
+    def predict(self, in_set, all_layers=False, classification=False):
         if all_layers:
             out = [np.array(in_set)]
             last_out = np.array(in_set)
 
             for i, layer in enumerate(self.layers):
+                # TODO: replace sigmoid?
                 last_out = sigmoid(np.dot(last_out, layer))
                 out.append(last_out)
 
@@ -48,61 +60,65 @@ class Network:
         else:
             last_out = self.add_bias(np.array(in_set))
             for i, layer in enumerate(self.layers):
+                # TODO: replace sigmoid?
                 last_out = self.add_bias(sigmoid(np.dot(last_out, layer)))
 
             return np.argmax(np.delete(last_out, [len(last_out[0])-1], axis=1), axis=1)
 
-    def add_bias(self, arr):
 
+    def add_bias(self, arr):
         b = np.array([np.ones(len(arr))])
         return np.concatenate((arr, b.T), axis=1)
 
-    def fit(self, X, y, iterations=10):
+
+    def fit(self, X, y, reset_layers=True):
         if len(X) != len(y):
             exit('TRAINING SET COUNT MISMATCH')
 
-        for iter in range(iterations):
+        # if reset_layers is set to False, reuse the
+        # layers from the previous call to fit
+        if reset_layers:
+            self.init_network_layers()
 
-            out = [np.array(X)]
-            last_out = self.add_bias(np.array(X))
+        X_to_arr = np.array(X)
+
+        for iter in range(self.iterations):
+            out = [X_to_arr]
+            last_out = self.add_bias(X_to_arr)
 
             for i, layer in enumerate(self.layers):
+                # TODO: replace sigmoid?
                 last_out = self.add_bias(sigmoid(np.dot(last_out, layer)))
                 out.append(last_out)
 
             out_layers = out
-
             out_layers[-1] = np.delete(out_layers[-1], [len(out_layers[-1][0])-1], axis=1)
             #print(out_layers[-1].shape)
-
             last_error = y - out_layers[-1]
 
+            # TODO: replace sigmoid?
             last_delta = last_error * sigmoid(out_layers[-1], True)
-
             deltas = [last_delta]
 
             flag = False
-
             for k in range(len(self.layers) - 1, 0, -1):
                 #print(k, "ld", last_delta, '\nld shape', last_delta.shape, "layer shape", self.layers[k].shape)
-
                 if flag:
                     last_delta = last_delta.T[:-1].T
                 else:
                     flag = True
+
                 layer_error = last_delta.dot(self.layers[k].T)
 
+                # TODO: replace sigmoid?
                 layer_delta = layer_error * sigmoid(out_layers[k], True)
-
                 last_delta = layer_delta
-
                 deltas.append(layer_delta)
 
 
             flag = False
             for k in range(len(self.layers) - 1, 0, -1):
                 #print("d", deltas[-k-1].shape, "o", out_layers[k].shape, "w", self.layers[k].shape)
-
                 change = out_layers[k].T.dot(deltas[-k - 1])
 
                 if flag:
@@ -110,18 +126,17 @@ class Network:
                 else:
                     flag = True
 
-
-
                 #print("change", change.shape)
-
                 self.layers[k] += change*self.learning_rate
+
+
 
     def storage(self):
         out = []
         for layer in self.layers:
             out.append(layer.tolist())
-
         return out
+
 
     def load(self, layers):
         out = []
@@ -129,8 +144,10 @@ class Network:
             out.append(np.array(layer))
         self.layers = out
 
+
     def __repr__(self):
         return str(self.layers)
+
 
     def __str__(self):
         return self.__repr__()
--- README.md
+++ README.md
@@ -1,2 +1,105 @@
 # InformationPain
 Data Mining group project
+
+
+**Requirements**
+```
+python              --  3.6
+jupyter notebook    --  5.41
+scipy               --  1.0.0
+sklearn             --  0.19.1
+pandas              --  0.22.0
+numpy               --  1.14.2
+tqdm                --  4.23.0
+```
+
+To use our models, it's as simple as importing and instantiating the class, call fit with the feature and their labels, and then call predict with the test features.
+
+**ID3**
+```
+from ID3 import ID3
+id3 = ID3()
+id3.fit(features_train, labels_train)
+predictions = id3.predict(features_test)
+```
+
+**Naive Bayes**
+```
+from Naive_Bayes import NaiveBayesClassifier
+nbc = NaiveBayesClassifier()
+nbc.fit(features_train, labels_train)
+nbc_prediction = nbc.predict(features_test)
+print(classification_report(labels_test, nbc_prediction))
+```
+
+**Neural Network**
+- The training labels must be one hot encodings, even if they are numeric (in the future the network can do the conversion for the user)
+- If the features are nominal then they will have to be converted into one hot encodings as well
+- When instantiating the network you have to give the correct shape for the input and output layers.
+- Any integer between the first and last element of the layer_structures corresponds to the amount of neurons in the relative hidden layer
+- The network defaults to sigmoid for its activation function, but relu can be imported and used.
+- If you need to retrain the network, create a new object; as of now the weights don't reset
+
+```
+from Neural_Network import Network, relu
+# convert labels to one hots for the neural network
+labels_train_one_hot = pd.get_dummies(labels_train).as_matrix()
+
+neuralnet = Network(layer_structures=[40, 10, 10, 3], activation_function=relu)
+neuralnet.fit(features_train, labels_train_one_hot, iterations=5000)
+neuralnet_prediction = neuralnet.predict(features_test, classification=True)
+```
+
+
+**Putting it all together example:**
+```
+features, labels = make_classification(n_samples=3000, n_classes=3, n_informative=20,
+                                       			n_features=30, class_sep=1.5)
+kf = KFold(n_splits=10, shuffle=True)
+
+id3 = ID3()
+naive_bayes = NaiveBayesClassifier()
+
+labels_true = []
+id3_predicted = []
+naive_bayes_predicted = []
+neuralnet_predicted = []
+
+for train_index, test_index in kf.split(features):
+    features_train, features_test = features[train_index], features[test_index]
+    labels_train, labels_test = labels[train_index], labels[test_index]
+
+    # save all the of test labels
+    labels_true.extend(labels_test)
+
+    # convert labels to one hots for the neural network
+    # features are numeric, so they don't have to be converted
+    labels_train_one_hot = pd.get_dummies(labels_train).as_matrix()
+
+    # recreate Neural Network, for now it doesn't reset its weight, thus make a new one
+    # 30 feature neurons, 2 hidden layers with 15 neurons each, 3 output neurons
+    neuralnet = Network(layer_structures=[30, 15, 15, 3], activation_function=relu)
+
+    # train models
+    id3.fit(features_train, labels_train)
+    naive_bayes.fit(features_train, labels_train)
+    neuralnet.fit(features_train, labels_train_one_hot, iterations=5000)
+
+    # get predictions
+    id3_predicted.extend(id3.predict(features_test))
+    naive_bayes_predicted.extend(naive_bayes.predict(features_test))
+    neuralnet_predicted.extend(neuralnet.predict(features_test, classification=True))
+
+
+print('\n\nID3 Decision Tree:')
+print('Accuracy: {:.2f}'.format(accuracy_score(labels_true, id3_predicted)))
+print(classification_report(labels_true, id3_predicted))
+
+print('\n\nNaive Bayes:')
+print('Accuracy: {:.2f}'.format(accuracy_score(labels_true, naive_bayes_predicted)))
+print(classification_report(labels_true, naive_bayes_predicted))
+
+print('\n\nOur Neural Network:')
+print('Accuracy: {:.2f}'.format(accuracy_score(labels_true, neuralnet_predicted)))
+print(classification_report(labels_true, neuralnet_predicted))
+```
